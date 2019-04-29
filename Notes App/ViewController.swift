0957c150738f56005d5f962b95f2aab9231e3d82
GJteVelde <gerjan.te.velde@gmail.com>
2019-04-29 13:44:35 +0200

    Initial Commit

---
+++ Notes App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes App
+//
+//  Created by Gerjan te Velde on 29/04/2019.
+//  Copyright © 2019 Gerjan te Velde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
