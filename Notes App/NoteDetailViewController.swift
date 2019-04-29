ae8b3f6d8ee60cf7a57601a115b13c0d6a160a15
GJteVelde <gerjan.te.velde@gmail.com>
2019-04-29 19:56:11 +0200

    Adjust frame of noteTextView to keyboard changes and add done barButtonItem to send resignFirstResponder() message to noteTextView.

--- Notes App/NoteDetailViewController.swift
+++ Notes App/NoteDetailViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class NoteDetailViewController: UIViewController {
+class NoteDetailViewController: UIViewController, UITextViewDelegate {
 
     //MARK: - Objects and Properties
     var note: NSAttributedString?
     weak var delegate: NotesTableViewController!
     
     @IBOutlet weak var noteTextView: UITextView!
+    var doneBarButton: UIBarButtonItem!
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        doneBarButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneBarButtonTapped))
+        navigationItem.rightBarButtonItem = doneBarButton
+        
+        if note == nil {
+            noteTextView.becomeFirstResponder()
+            doneBarButton.isEnabled = true
+        } else {
+            doneBarButton.isEnabled = false
+        }
+        
+        noteTextView.delegate = self
         noteTextView.allowsEditingTextAttributes = true
         noteTextView.attributedText = note ?? NSAttributedString()
     }
@@ -28,5 +44,33 @@ class NoteDetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         
         delegate.newOrEditedNote(noteTextView.attributedText!)
+    }
+    
+    //MARK: - Methods
+    @objc func adjustKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        let keyboardScreenEndFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            noteTextView.contentInset = UIEdgeInsets.zero
+        } else {
+            noteTextView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        noteTextView.scrollIndicatorInsets = noteTextView.contentInset
+        
+        let selectedRange = noteTextView.selectedRange
+        noteTextView.scrollRangeToVisible(selectedRange)
+    }
+    
+    @objc func doneBarButtonTapped() {
+        noteTextView.resignFirstResponder()
+        doneBarButton.isEnabled = false
+    }
+    
+    //MARK: - Delegate Methods
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        doneBarButton.isEnabled = true
     }
 }
